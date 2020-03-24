ad675312ca07734d8fed206f74a667d3c9c047c3
Xavier Carty <xaviercarty@gmail.com>
2020-03-24 17:20:50 -0400

    code

--- lib/models/Reservation.rb
+++ lib/models/Reservation.rb
@@ -1,14 +1,15 @@
 class Reservation < ActiveRecord::Base
-
     belongs_to :restaurant
-
+    belongs_to :user
+    @prompt = TTY::Prompt.new
     def self.show_open
         open = Reservation.all.where(user_id: nil).map do |reso|
-            reso.restaurant.name
+            reso
         end
     end
-
-    #
-
-
+    def self.book(user)
+        reservation = @prompt.select("Where would you like to dine?", show_open)
+        reservation.user = user
+        reservation
+    end
 end
